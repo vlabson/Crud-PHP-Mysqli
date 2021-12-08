84020b650102ceb121685fc7e913d972bd00b466
Vlabson Viana <vlabsonsviana@gmail.com>
2021-12-08 00:26:18 -0300

    atualização
    correção de link pois o mesmo estava errado

--- proc_edit_usuario.php
+++ proc_edit_usuario.php
@@ -8,15 +8,19 @@ $id = $_POST["id"];
 $nome = $_POST["nome"];
 $email = $_POST["email"];
 
+/*
 echo "ID : $id <br>";
 echo "Nome : $nome <br>";
 echo "Email : $email <br>";
+*/
 
 //Cria uma query UPDATE ( para editar ) a mesma ja recebe os valores e armazena a query na variavel assim de otmizar codigos
+
 $result_usuario = "UPDATE usuarios SET nome='$nome' , email ='$email', data_de_modificacao = NOW() where id = '$id'";
 
 //executa a query ja criada acima e armazenada na variavel 
 //como parametro é passado a variavel CONN para criar a conexao e a query para armazenar no banco.
+
 $resultado_usuario = mysqli_query($conn, $result_usuario);
 
 
@@ -26,7 +30,7 @@ if(mysqli_affected_rows($conn)){
     header("location: listar.php");
 }else{
     $_SESSION['msg'] = "<p style='color:red;'>Usuário não foi editado com Sucesso!!</p>";
-    header("location: editar.php?id=$id");
+    header("location: edit_usuario.php?id=$id");
 }
 
 ?>
